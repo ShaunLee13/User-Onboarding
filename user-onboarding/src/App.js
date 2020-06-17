96c52f812bcc782eb88c59b6ab7282c207c7d757
Shaun Lee <shaundlee.0103@gmail.com>
2020-06-17 16:48:37 -0400

    Added values to form and built event handlers

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -3,21 +3,60 @@ import './App.css';
 import Form from './components/Form';
 import * as Yup from 'yup'
 
-
-
-
 const initialDisabled = true
+const initialFormValues = {
+  username: '',
+  email: '',
+  password: '',
+  terms: false
+}
 
 
 function App() {
   const [ disable, setDisable ] = useState(initialDisabled)
+  const [ formValues, setFormValues ] = useState(initialFormValues)
+  const [ member, setMember ] = useState([])
 
+
+
+  const onInputChange = evt => {
+    const { name, value } = evt.target
+
+    setFormValues({
+      ...formValues, [name]:value
+    })
+  }
+
+  const onCheckboxChange = evt => {
+    const { name, checked } = evt.target
+
+    setFormValues({
+      ...formValues, [name]:checked
+    })
+  }
+
+  const onSubmit = evt => {
+    evt.preventDefault()
+
+    const newMember = {
+      username: formValues.username.trim(),
+      email: formValues.email.trim(),
+      password: formValues.password.trim(),
+      terms: formValues.terms
+    }
+  }
 
 
   return (
     <div className="App">
       <header className="App-header">
-        <Form disabled={disable}/>
+        <Form 
+        disabled={disable} 
+        values={formValues} 
+        onInputChange={onInputChange}
+        onCheckboxChange={onCheckboxChange}
+        onSubmit={onSubmit}
+        />
       </header>
     </div>
   );
--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -2,28 +2,56 @@ import React from 'react'
 
 const Form = props => {
     const {
-        disabled
+        values,
+        onInputChange,
+        onSubmit,
+        disabled,
+        onCheckboxChange
     } = props
 
+    console.log(values)
     return (
-        <div>
+        <form onSubmit={onSubmit}>
             <p>The form is connected!</p>
             
             <div className='Form-Container'>
                 <label>Name:&nbsp;
-                    <input />
+                    <input
+                    type='text'
+                    name='username'
+                    value={values.username}
+                    placeholder='Please Enter Name'
+                    onChange={onInputChange}
+                    />
                 </label>
 
                 <label>Email:&nbsp;
-                    <input />
+                    <input 
+                    type='email'
+                    name='email'
+                    value={values.email}
+                    placeholder="Enter valid Email Address"
+                    onChange={onInputChange}
+                    />
                 </label>
 
                 <label>Password:&nbsp;
-                    <input />
+                    <input 
+                    type='password'
+                    name='password'
+                    value={values.password}
+                    placeholder='Please enter a Password'
+                    onChange={onInputChange}
+                    />
                 </label>
 
                 <label className='ToS'>
-                    <input type='checkbox' />
+                    <input 
+                    name='terms'
+                    type='checkbox'
+                    checked={values.terms}
+                    onChange={onCheckboxChange}
+                    />
                     I have read and agree to the Terms of Service&nbsp;
                 </label>
             </div>
@@ -34,7 +62,7 @@ const Form = props => {
                 disabled={disabled}
                 >Apply</button>
             </div>
-        </div>
+        </form>
         
     )
 }
